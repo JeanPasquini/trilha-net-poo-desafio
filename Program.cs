0ced470fa4599731b2ba2d436b507577634400f2
JeanPasquini <pasquinijean@hotmail.com>
2024-05-13 10:37:26 -0300

    2024-05-13 DESAFIO POO
    CONLCUINDO DESAFIO DE PROGRAMAÇÃO ORIENTADA A OBJETO - DESAFIO DE SMARTPHONE - DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+            Nokia nokia = new Nokia("123456789", "Nokia XYZ", "IMEI123", 64);
+            Console.WriteLine("Informações do Nokia:");
+            Console.WriteLine($"Número: {nokia.Numero}");
+            Console.WriteLine($"Modelo: {nokia.Modelo}");
+            Console.WriteLine($"IMEI: {nokia.IMEI}");
+            Console.WriteLine($"Memória: {nokia.Memoria}GB");
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+            
+            Console.WriteLine();
+
+            Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 128);
+            Console.WriteLine("Informações do iPhone:");
+            Console.WriteLine($"Número: {iphone.Numero}");
+            Console.WriteLine($"Modelo: {iphone.Modelo}");
+            Console.WriteLine($"IMEI: {iphone.IMEI}");
+            Console.WriteLine($"Memória: {iphone.Memoria}GB");
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
